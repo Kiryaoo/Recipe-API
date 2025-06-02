81b46c6631d3fe5fcb84b147f378363683c38cdc
kirillka.k@gmail.com <108414175+Kiryaoo@users.noreply.github.com>
2025-06-02 20:47:15 +0300

    Added:
    - user
    -user tests
    -authentication
    -managing profile

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -31,4 +32,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     ),
+    path('api/user/', include('user.urls')),
 ]
