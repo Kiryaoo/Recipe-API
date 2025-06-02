da4169a3ca40a9e6aaa29bbfedd433eeb2761af0
kirillka.k@gmail.com <108414175+Kiryaoo@users.noreply.github.com>
2025-06-02 19:00:30 +0300

    Configured URLs

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,3 +19,26 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from core import views as core_views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    ),
+]
